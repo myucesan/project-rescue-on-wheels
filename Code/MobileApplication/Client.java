010b61b11d6d7e544f89c1e1b434ff606b2d3c64
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2018-09-27 23:54:27 +0200

    Add new file

---
+++ Code/MobileApplication/Client.java
@@ -0,0 +1,76 @@
+
+
+public class Client implements Serializable {
+
+    private DatagramSocket socket;
+    private String key;
+    private InetAddress host;
+    private int port;
+    private byte[] data;
+    private DatagramPacket packet;
+
+
+    public Client(InetAddress host, int port) {
+        this.host = host;
+        this.port = port;
+
+        try {
+            socket = new DatagramSocket();
+
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getHost() {
+        return host.toString();
+    }
+
+    public void setHost(InetAddress host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void getPort(int port) {
+        this.port = port;
+    }
+
+    public DatagramSocket getSocket() {
+        return socket;
+    }
+
+    public void setKey(String string) {
+        key = string;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public void sendData(String message) {
+        try {
+            data = message.getBytes();
+            packet = new DatagramPacket(data, data.length, host, port);
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        socket.close();
+    }
+
+}
