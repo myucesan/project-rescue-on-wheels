8f817a35182e3702a8c34b73ed070b2643faf271
Shiro <damian190085@gmail.com>
2018-10-02 12:17:56 +0200

    Update srf02.test
    Improved code, ready for testing.

--- TestCode/SFR02Test.py
+++ TestCode/SFR02Test.py
@@ -4,21 +4,28 @@ import smbus
 import time
 from threading import *
 
-bus = smbus.SMBus(1)
-#Adress of the I2C_SLAVE
-SLAVE_ADDRESS = 0x70
-cmMeasure = [00, 0x51] #command for measurement in cm
-readLowByte = [0x03]
-lowByte = 3
+class srf02:
+    def _init_(self):
+        self.bus = smbus.SMBus(1)
+        #Adress of the I2C_SLAVE
+        self.SLAVE_ADDRESS = 0x70
+        self.cmMeasure = 81 #command for measurement in cm
+        #readLowByte = [0x03]
+        #self.lowByte = 3
 
-def main():
-    time.sleep(1)
-    while True:
-        bus.write_i2c_block_data(SLAVE_ADDRESS, 0, cmMeasure)
-        time.sleep(1)
-        bus.write_i2c_block_data(SLAVE_ADDRESS, 0, readLowByte)
-        low = bus.read_byte_data(SLAVE_ADDRESS, 0)
-        print(low)
-        time.sleep(2)
+    def calculateDistance(self):
+        while True:
+            self.bus.write_byte_data(self.SLAVE_ADDRESS, 0 ,self.cmMeasure)
+            time.sleep(1)
+            distance = self.bus.read_word_data(self.SLAVE_ADDRESS, 2) / 255
+            mindistance = self.bus.read_word_data(self.SLAVE_ADDRESS, 4) / 255
+            print(distance + " -  Minimal Distance:" + mindistance)
+            time.sleep(2)
+    
+            #bus.write_i2c_block_data(SLAVE_ADDRESS, 0, readLowByte)
+            #low = bus.read_byte_data(SLAVE_ADDRESS, 0)
+            #print(low)
+            #time.sleep(1)
 
-main()
+srf02().calculateDistance()
+
