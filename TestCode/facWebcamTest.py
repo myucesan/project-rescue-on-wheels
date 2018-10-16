0fd86976d89dd076b9cd9ef6463b41730125c011
@hoogd1 <damian.de.hoog@hva.nl>
2018-10-16 09:28:57 +0000

    fixed out of loop error

--- TestCode/facWebcamTest.py
+++ TestCode/facWebcamTest.py
@@ -23,16 +23,16 @@ while true:
 		flags=cv2.cv.CV_HAAR_SCALE_IMAGE
 	)
 
-#Drawing the rectangle around the faces
-for (x, y, w, h) in faces:
-	cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	#Drawing the rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-#display the resulting frame
-cv2.imshow('Video', frame)
+	#display the resulting frame
+	cv2.imshow('Video', frame)
 
-# Press 'q' to stop the script running
-if cv2.waitKey(1) & 0xFF == ord('q'):
-	break
+	# Press 'q' to stop the script running
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 #when everything is done, release the capture
 video_capture.release()
