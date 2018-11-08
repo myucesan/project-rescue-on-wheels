d66b39f634784b0fd7bc031beba9ef267f7d1291
Y.C. Schermer <yoshio.schermer@hva.nl>
2018-11-08 09:37:31 +0000

    Fix steering

--- Code/Rover/MotorInitialization.py
+++ Code/Rover/MotorInitialization.py
@@ -2,6 +2,7 @@
 import RPi.GPIO as gpio
 import smbus
 import time
+from Socket import *
 
 class MotorInitialization:
 
@@ -19,13 +20,19 @@ class MotorInitialization:
          self.Softstart = [0x91,100,0]
          #Address to access motor
          self.SLAVE_ADDRESS = 0x32
-         gpio.setmode(gpio.BCM)
-         gpio.setup(17, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+	 self.prevValue = None
+	 self.prevSpeed = None
+	 self.socket = Socket()
 
+    def setUp(self):
+	gpio.setmode(gpio.BCM)
+	gpio.setup(17, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+	self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.Totalpower)
+	self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.Softstart)
 
     def forward(self):
          #Drive forward
-         self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.MotorFD)
+	self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.MotorFD)
 
     def backward(self):
          #Drive backward
@@ -38,7 +45,6 @@ class MotorInitialization:
     def right(self):
          #Drive right forward
          self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.MotorR)
-
     def stop(self):
          #Stop driving
          self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.MotorST)
@@ -46,7 +52,26 @@ class MotorInitialization:
     def setSpeed(self, speed):
          #Set motor speed
          self.Totalpower[1] = speed
+	 print(self.Totalpower[1])
+	 time.sleep(1)	
          self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.Totalpower)
          self.bus.write_i2c_block_data(self.SLAVE_ADDRESS, 0, self.Softstart)
 
+    def drive(self):
+	
+    	while True:
+        	self.socket.receiveValues()
+        	if self.prevValue == None or self.prevValue != self.socket.state:
+                	if self.socket.state == "forward":
+                        	self.forward()
+                	if self.socket.state == "right":
+                        	self.right()
+                	if self.socket.state == "left":
+                        	self.left()
+                	if self.socket.state == "backward":
+                        	self.backward()
+                	if self.socket.state == "stop":
+                        	self.stop()
 
+		#if self.prevSpeed == None or self.prevSpeed != self.socket.speed:
+			#self.setSpeed(self.socket.speed)
--- Code/Rover/main.py
+++ Code/Rover/main.py
@@ -3,27 +3,10 @@ from MotorInitialization import *
 from threading import *
 MotorInitialization = MotorInitialization()
 
-def MotorControl():
-    socket = Socket()
-    while True:
-        socket.receiveValues()
-
-        if socket.state == "forward":
-            MotorInitialization.forward()
-        if socket.state == "right":
-            MotorInitialization.right()
-        if socket.state == "left":
-            MotorInitialization.left()
-        if socket.state == "backward":
-            MotorInitialization.backward()
-        if socket.state == "stop":
-            MotorInitialization.stop()
-
-        MotorInitialization.setSpeed(socket.speed)
-
 def main():
     print("main invoked")
-    thread = Thread(target=MotorControl)
+    thread = Thread(target=MotorInitialization.drive())
+    MotorInitialization.setUp()
     thread.start()
     thread.join()
 
