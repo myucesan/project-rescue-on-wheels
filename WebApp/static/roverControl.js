03b78fecba24cd3e6949b8268c8cf2206f9a2894
Mohamed el Hadiyen <mohamed.el.hadiyen@hva.nl>
2018-11-11 00:53:49 +0000

    Updates

--- WebApp/static/roverControl.js
+++ WebApp/static/roverControl.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
   var socket = io.connect('http://' + document.domain + ':' + location.port);
   var roverInfo = localStorage.getItem("RoverSelection");
+  var c = document.getElementById("canvastest");
+  var ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.moveTo(200, 100);
+  var x_b = 200;
+  var y_b = 100;
+  var hoek = Math.PI/2;
+  var v = 2;
   var control = {
 		"device": "Webapp",
 		"state": null,
                 "speed": 220,
-		"backtrack" : 0
+		"backtrack": 0
 	};
   socket.emit('roverConnection', roverInfo);
-
+  
   window.addEventListener("keydown", controlOnKey, false);
   window.addEventListener("keyup", stop, false);
 
   $('#driveback').on('click', function(){
-    console.log("TEST")
-    control.backtrack = 1
+    control.backtrack = 1;
     socket.emit('roverControl', JSON.stringify(control));
 });
 
@@ -45,7 +52,39 @@ $(document).ready(function() {
       socket.emit('roverControl', JSON.stringify(control));
     }
   }
+
   socket.on('lineDrawer', function(lineCalculation) {
-	console.log(lineCalculation)
+	var test = JSON.parse(lineCalculation);
+	var t = test["time"]*1000;
+	var direction = test["state"];
+	console.log(direction);
+	console.log(t);
+	switch (direction) {
+        	case "left":
+            		hoek += t*(Math.PI/1800);
+            		console.log("Hoek (links): " + hoek);
+            		break;
+        	case "forward":
+            		x_b += v * Math.cos(hoek);
+            		y_b -= v * Math.sin(hoek);
+            		ctx.lineTo(x_b, y_b);
+			ctx.strokeStyle = "white";
+            		ctx.stroke();
+            		console.log("Rechtdoor (x, y): " + x_b + ", " + y_b);
+            		break;
+        	case "right":
+	        	hoek += t*(-Math.PI/2600);
+            		console.log("Hoek (rechts): " + hoek);
+            		break;
+        	case "backward":
+           		x_b += v * Math.cos(hoek);
+            		y_b += v * Math.sin(hoek);
+            		ctx.lineTo(x_b, y_b);
+			ctx.strokeStyle = "white";
+            		ctx.stroke();
+            		console.log("Achteruit (x, y): " + x_b + ", " + y_b);
+            		break;
+    	}
+	
   }); 
 });
