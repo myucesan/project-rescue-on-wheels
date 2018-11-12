fd8563f8b1e530b214069f40b15277ec61e9bfa9
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2018-11-12 02:41:53 +0100

    Update Socket.py

--- Code/Rover/Socket.py
+++ Code/Rover/Socket.py
@@ -37,25 +37,25 @@ class Socket:
                 self.message, self.address = self.socket.recvfrom(1024)
 		if self.message is not None:
 			self.data = json.loads(self.message.decode())
-		if self.data != self.prevJson:
-  			self.device = self.data['device']
-                	self.state = self.data['state']
-                	self.speed = self.data['speed']
-                	self.backtrack = self.data['backtrack']
-                	if self.device == "Webapp":
-                            if self.backtrack == 0:
-                                if self.state != "stop":
-                                    self.begin = time.time()
-                                else:
-                                    self.end = time.time()
-                                    self.lineData["state"] = self.prevState
-                                    self.lineData["time"] = self.end - self.begin
-                                    self.begin = None
-                                    self.list.append(self.lineData)
-                                    self.socket.sendto(bytes(json.dumps(self.lineData), "utf-8"), self.address)
-    
-                        self.prevJson = self.data
-                        self.prevState = self.state
+    		if self.data != self.prevJson:
+      			self.device = self.data['device']
+                    	self.state = self.data['state']
+                    	self.speed = self.data['speed']
+                    	self.backtrack = self.data['backtrack']
+                    	if self.device == "Webapp":
+                                if self.backtrack == 0:
+                                    if self.state != "stop":
+                                        self.begin = time.time()
+                                    else:
+                                        self.end = time.time()
+                                        self.lineData["state"] = self.prevState
+                                        self.lineData["time"] = self.end - self.begin
+                                        self.begin = None
+                                        self.list.append(self.lineData)
+                                        self.socket.sendto(bytes(json.dumps(self.lineData), "utf-8"), self.address)
+        
+                            self.prevJson = self.data
+                            self.prevState = self.state
 			
 
             except socket.error:
