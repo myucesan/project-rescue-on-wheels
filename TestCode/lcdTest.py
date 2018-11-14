b264858103b199e4869397943f180a6a37c53595
Shiro <damian190085@gmail.com>
2018-11-14 12:01:24 +0100

    Finished test code for LCD.
    This test code should print the raspberry pi 3b modelname to the lcd screen. Included comments for clarification.

--- TestCode/lcdTest.py
+++ TestCode/lcdTest.py
@@ -56,6 +56,123 @@ def main():
 	#Initialise display
 	lcd_init()
 
-	#Toggle backlight on-off-on
-	lcd_backlight(true)
 
+	'''
+	#Toggle backlight on-off-on (implement later on)
+	lcd_backlight(True)
+	time.sleep(0.5)
+	lcd_backlight(False)
+	time.sleep(0.5)
+	lcd_backlight(True)
+	time.sleep(0.5)
+	'''
+
+	while True:
+
+		#Send some centred test
+		lcd_string("Raspberry Pi", LCD_LINE_1, 2)
+		lcd_string("Model B", LCD_LINE_2, 2)
+
+		time.sleep(3) #3 second delay
+
+		#Blank display
+		lcd_byte(0x01, LCD_CMD)
+
+		time.sleep(3) #3 second delay
+
+def lcd_init():
+	#initialise display
+	lcd_byte(0x33, LCD_CMD) # 110011 Initialise
+	lcd_byte(0x32, LCD_CMD) # 110010 Initialise
+	lcd_byte(0x06, LCD_CMD) #000110 Cursor move direction
+	lcd_byte(0x0C, LCD_CMD) #001100 Display On, Cursor Off, Blink Off
+	lcd_byte(0x28, LCD_CMD) #101000 Data length, number of lines, font size
+	lcd_byte(0x01, LCD_CMD) #000001 Clear display
+	time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+
+	#Send byte to data pins
+	#bits = data
+	#mode = True for character
+	#		False for command
+
+	GPIO.output(LCD_RS, mode) #RS
+
+	# High bits
+	GPIO.output(LCD_D4, False)
+	GPIO.output(LCD_D5, False)
+	GPIO.output(LCD_D6, False)
+	GPIO.output(LCD_D7, False)
+	if bits&0x10==0x10:
+		GPIO.output(LCD_D4, True)
+	if bits&0x20==0x20:
+		GPIO.output(LCD_D5, True)
+	if bits&0x40==0x40:
+		GPIO.output(LCD_D6, True)
+	if bits&0x80==0x80:
+		GPIO.output(LCD_D7, True)
+
+	#toggle 'Enable' pin
+	lcd_toggle_enable()
+
+	#Low bits
+	GPIO.output(LCD_D4, False)
+	GPIO.output(LCD_D5, False)
+	GPIO.output(LCD_D6, False)
+	GPIO.output(LCD_D7, False)
+	if bits & 0x01 == 0x01:
+		GPIO.output(LCD_D4, True)
+	if bits & 0x02 == 0x02:
+		GPIO.output(LCD_D5, True)
+	if bits & 0x04 == 0x04:
+		GPIO.output(LCD_D6, True)
+	if bits & 0x08 == 0x08:
+		GPIO.output(LCD_D7, True)
+
+	#Toggle 'Enable' pin
+	lcd_toggle_enable()
+
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+
+
+def lcd_string(message, line, style):
+	# Send string to display
+	# style=1 Left justified
+	# style=2 Centred
+	# style=3 Right justified
+
+	if style == 1:
+		message = message.ljust(LCD_WIDTH, " ")
+	elif style == 2:
+		message = message.center(LCD_WIDTH, " ")
+	elif style == 3:
+		message = message.rjust(LCD_WIDTH, " ")
+
+	lcd_byte(line, LCD_CMD)
+
+	for i in range(LCD_WIDTH):
+		lcd_byte(ord(message[i]), LCD_CHR)
+
+
+def lcd_backlight(flag):
+	# Toggle backlight on-off-on
+	GPIO.output(LED_ON, flag)
+
+
+if __name__ == '__main__':
+
+	try:
+		main()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		lcd_byte(0x01, LCD_CMD)
+		lcd_string("Goodbye!", LCD_LINE_1, 2)
+		GPIO.cleanup()
