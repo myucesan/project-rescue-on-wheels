4a35ed2ecba386018672e31f1f2b218a70338658
Y.C. Schermer <yoshio.schermer@hva.nl>
2018-12-07 13:59:28 +0000

    added code for servo based on servoblaster though, and aiortctest

---
+++ servo.py
@@ -0,0 +1,28 @@
+import time
+import os
+from threading import Thread
+
+class Servo:
+    
+    _PIN = 15
+    _MAX = 2500
+    _MID_MAX = 2000
+    _MID = 1500
+    _MID_MIN = 1000
+    _MIN = 500
+    _start = True
+
+    def set(self, value):
+        os.system("echo " + "P1-" + str(self._PIN) + "=" + str(value) + "us > /dev/servoblaster")
+      
+    def run(self):
+        while self._start:
+            self.set(self._MID_MAX)
+            time.sleep(1)
+            self.set(self._MID_MIN)
+            time.sleep(1)
+
+    def stop(self):
+        self._start = False
+
+
