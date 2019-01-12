b284743b28e7b83dc09f9c43d734c3c0ce0671c0
C.M. Yücesan <mustafa.yucesan@hva.nl>
2019-01-12 23:12:09 +0100

    Added comments to clarify code

--- LCD.py
+++ LCD.py
@@ -5,7 +5,8 @@ import time
 
 
 class LCD:
-    _LCD_RS = None
+    # Below variables corresponding to the LCD pins
+    _LCD_RS = None # signal for selecting registers
     _LCD_E = None
     _LCD_D4 = None
     _LCD_D5 = None
@@ -23,7 +24,8 @@ class LCD:
     _E_DELAY = None
 
     _DICT = None
-
+    
+    #Initialising variables
     def __init__(self):
 
         self._LCD_RS = 7
@@ -42,9 +44,11 @@ class LCD:
 
         self._E_PULSE = 0.0005
         self._E_DELAY = 0.0005
-
+        
+        # Variable representing the first and second line of the LCD
         self._DICT = {self._LCD_LINE_1: None, self._LCD_LINE_2: None}
 
+        # Sets numbering mode to BOARD and setting corresponding pins to output.
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self._LCD_E, GPIO.OUT)
         GPIO.setup(self._LCD_RS, GPIO.OUT)
@@ -53,6 +57,7 @@ class LCD:
         GPIO.setup(self._LCD_D6, GPIO.OUT)
         GPIO.setup(self._LCD_D7, GPIO.OUT)
 
+    # Enables or disables a bit based based on the bits that are given in hexadecimal
     def lcd_byte(self, bits, mode):
         GPIO.output(self._LCD_RS, mode)
 
@@ -90,7 +95,7 @@ class LCD:
 
             # Toggle 'Enable' pin
         self.lcd_toggle_enable()
-
+    
     def lcd_toggle_enable(self):
         # Toggle enable
         time.sleep(self._E_DELAY)
@@ -98,7 +103,7 @@ class LCD:
         time.sleep(self._E_PULSE)
         GPIO.output(self._LCD_E, False)
         time.sleep(self._E_DELAY)
-
+    #clears lcd
     def clear_string(self):
         self.lcd_byte(0x01, self._LCD_CMD)
         time.sleep(1)
@@ -116,6 +121,7 @@ class LCD:
             for b in range(self._LCD_WIDTH):
                 self.lcd_byte(ord(self._DICT[i][b]), self._LCD_CHR)
 
+    # Starts the lcd, enables bits on the screen on some conditions.
     def start(self):
         self.lcd_byte(0x33, self._LCD_CMD)  # 110011 Initialise
         self.lcd_byte(0x32, self._LCD_CMD)  # 110010 Initialise
